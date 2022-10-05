3db2501848e5d5859bf5c114a7bf980963cfc89d
Renato Boscarino <49559182+dragontyle@users.noreply.github.com>
2022-10-05 16:55:18 -0300

    Update exercicio6_lista2.py

--- exercicio6_lista2.py
+++ exercicio6_lista2.py
@@ -3,7 +3,7 @@
 
 # Definindo a variável
 somaquadrados = 0
-# For para realizar a criação da lista para digitação dos 10 números inteiros e in para realizar se está dentro do for
+# For para realizar a criação da lista para digitação dos 10 números inteiros e in para realizar se está dentro do for e realizar a soma dos quadrados dos números inteiros
 for i in range(10):
     somaquadrados += int(input(f"Digite o {i+1}º numero inteiro: ")) ** 2
 print(f"A soma dos quadrados dos numeros digitados é {somaquadrados}")
